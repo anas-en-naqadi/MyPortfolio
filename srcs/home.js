29540c59c37fa31b6e02932fe123c44c0c4def3b
ANAS EN-NAQADI <anas.ennaqadi0@gmail.com>
2024-09-15 19:53:00 +0100

    updates

--- srcs/home.js
+++ srcs/home.js
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-
-let dark = false;
-document
-  .querySelector("#nav-list li:last-child")
-  .addEventListener("click", function () {
-    dark = !dark;
-    if (dark) {
-      document.querySelector("section h1").style.color = "white";
-      document.querySelector("section h3").style.color = "white";
-      document.querySelector(" header nav h3").style.color = "orange";
-      document.body.classList.add("dark-mode");
-      document.querySelector(
-        "#nav-list li:last-child  i:first-child"
-      ).style.display = "none";
-
-      document.querySelector(
-        "#nav-list li:last-child  i:last-child"
-      ).style.display = "flex";
-    } else {
-      document.querySelector("section h3").style.color = "";
-      document.querySelector("section h1").style.color = "";
-      document.querySelector(" header nav h3").style.color = "";
-      document.body.classList.remove("dark-mode");
-      document.querySelector(
-        "#nav-list li:last-child  i:first-child"
-      ).style.display = "flex";
-      document.querySelector(
-        "#nav-list li:last-child  i:last-child"
-      ).style.display = "none";
-    }
-  });
-document.querySelector("nav ul li:first-child a").style.color = "orange";
-
-let test = false;
-document.querySelector(".checkbtn").addEventListener("click", function () {
-  test = !test;
-  if (test) {
-    document.querySelector("#nav-list").style.display = "flex";
-  } else {
-    document.querySelector("#nav-list").style.display = "none";
-  }
-});
-function colorLink() {
-  var ul = document.getElementById("nav-list");
-  var li = ul.querySelectorAll("li");
-
-  for (let a of li) {
-    a.querySelector("a").addEventListener("click", function () {
-      for (let a of li) {
-        a.querySelector("a").style.color = "";
-      }
-      a.querySelector("a").style.color = "orange";
-    });
-  }
-}
-colorLink();
-=======
 
 // Add the move-animation class to each image
 // const images = document.querySelectorAll('#animated-section img');
@@ -190,4 +132,3 @@ document.getElementById("send-btn").addEventListener("click", function (event) {
     );
 });
           
->>>>>>> 5975270 (updates)
